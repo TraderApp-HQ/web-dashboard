2d482a75a07acadde9fc173745b59916ecdbbb63
Dike Ajah <ekidhaja@gmail.com>
2024-07-28 01:03:32 +0100

    init setup and cleanup

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+// module.exports = {
+//   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+//   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+//   moduleNameMapper: {
+//     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+//   },
+// };
+
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/src/$1',  // Handles path aliases starting with '@'
+  },
+  moduleDirectories: ["node_modules", "<rootDir>/src"],
+};
+
+module.exports = createJestConfig(customJestConfig);
