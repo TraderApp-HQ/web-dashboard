f07e0d7b5604bcdaeb6289e0454498aaa567e932
Ndife Uchenna <32971118+Ndife@users.noreply.github.com>
2024-08-23 10:57:10 +0100

    add hooks to copy text to clipboard

---
+++ src/hooks/useClipboard/index.ts
@@ -0,0 +1,17 @@
+import { useState, useCallback } from "react";
+
+export const useClipboard = () => {
+	const [copyMessage, setCopyMessage] = useState<string | undefined>(undefined);
+
+	const copyToClipboard = useCallback(async (text: string) => {
+		try {
+			await navigator.clipboard.writeText(text);
+			setCopyMessage("Copied to clipboard!");
+		} catch (error) {
+			console.error("Failed to copy text: ", error);
+			setCopyMessage("Failed to copy.");
+		}
+	}, []);
+
+	return { copyToClipboard, copyMessage, setCopyMessage };
+};
