b6a5468282c30b6b286c08ef4422532e88255084
Ndife Uchenna <32971118+Ndife@users.noreply.github.com>
2024-08-23 10:56:04 +0100

    separate exchange connection components

---
+++ src/components/AccountLayout/TradeCenter/ManualConnection/index.tsx
@@ -0,0 +1,99 @@
+import SelectBox from "~/components/common/SelectBox";
+import InputField from "~/components/common/InputField";
+import Button from "~/components/AccountLayout/Button";
+import Link from "next/link";
+import ExchangeCopyIcon from "~/components/icons/ExchangeCopyIcon";
+import { ISelectBoxOption } from "~/components/interfaces";
+import { useClipboard } from "~/hooks/useClipboard";
+import Toast from "~/components/common/Toast";
+
+interface ManualConnectionProps {
+	selectedExchange: ISelectBoxOption | undefined;
+	exchangeOptions: ISelectBoxOption[];
+	setSelectedExchange: (option: ISelectBoxOption | undefined) => void;
+	setApiKey: (apiKey: string | undefined) => void;
+	setSecretKey: (secretKey: string | undefined) => void;
+	isSubmitDisabled: boolean;
+	ipString: string;
+}
+
+const ManualConnection: React.FC<ManualConnectionProps> = ({
+	selectedExchange,
+	exchangeOptions,
+	setSelectedExchange,
+	setApiKey,
+	setSecretKey,
+	isSubmitDisabled,
+	ipString,
+}) => {
+	const { copyToClipboard, copyMessage } = useClipboard();
+
+	const handleExchangeCopy = () => {
+		copyToClipboard(ipString);
+	};
+
+	return (
+		<div className="flex flex-col gap-y-4">
+			<SelectBox
+				option={selectedExchange}
+				isSearchable
+				labelText="Exchange"
+				options={exchangeOptions}
+				placeholder="Select Exchange Channel"
+				setOption={setSelectedExchange}
+			/>
+			<InputField
+				type="text"
+				labelText="API Keys"
+				props={{ name: "apikey" }}
+				placeholder="Enter API Keys"
+				onChange={setApiKey}
+			/>
+			<InputField
+				type="text"
+				labelText="Secret Keys"
+				props={{ name: "secretkey" }}
+				placeholder="Enter Secret Keys"
+				onChange={setSecretKey}
+			/>
+			<div>
+				<h3 className="text-gray-950 text-base font-bold uppercase">Important</h3>
+				<span className="text-zinc-500 text-sm font-bold leading-tight">
+					You must add TraderApp IP Address from below to your list of trusted IPs.
+					Otherwise, if there are some days of inactivity, your IP key will be deleted by
+					the exchange.
+				</span>
+			</div>
+			<InputField
+				type="text"
+				icon={{ name: <ExchangeCopyIcon />, onClick: handleExchangeCopy }}
+				labelText="IP Address"
+				props={{ name: "ipaddress", disabled: true }}
+				placeholder={ipString}
+			/>
+			<Button
+				disabled={isSubmitDisabled}
+				type="submit"
+				fluid
+				className="mt-2 flex justify-center"
+				innerClassName="px-[20%] py-4 capitalize"
+			>
+				Connect
+			</Button>
+			<Link className="mt-2 flex justify-center text-blue-800" href={""}>
+				How to Connect
+			</Link>
+			{copyMessage && (
+				<Toast
+					type="success"
+					variant="outlined"
+					title="Success"
+					message={copyMessage}
+					autoVanish
+				/>
+			)}
+		</div>
+	);
+};
+
+export default ManualConnection;
--- src/pages/account/trade-center/exchanges/connect/index.tsx
+++ src/pages/account/trade-center/exchanges/connect/index.tsx
@@ -1,20 +1,16 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import Link from "next/link";
 import MessageModal from "~/components/Modal/MessageModal";
-import Button from "~/components/AccountLayout/Button";
-import InputField from "~/components/common/InputField";
 import SuccessIcon from "~/components/icons/SuccessIcon";
-import SelectBox from "~/components/common/SelectBox";
-import ExchangeCopyIcon from "~/components/icons/ExchangeCopyIcon";
 import Modal from "~/components/Modal";
 import ExchangeTile from "~/components/AccountLayout/TradeCenter/ExchangeTile";
 import ContentTab from "~/components/AccountLayout/ContentTab";
 import { NestedTradeCenterLayout } from "../..";
-import OrderedListTile from "~/components/AccountLayout/OrderedListTile";
 import useExchanges from "~/hooks/useExchanges";
 import { ISelectBoxOption } from "~/components/interfaces";
 import { TradeStatus } from "~/apis/handlers/assets/enums";
+import FastConnection from "~/components/AccountLayout/TradeCenter/FastConnection";
+import ManualConnection from "~/components/AccountLayout/TradeCenter/ManualConnection";
 
 const ExchangeConnection = () => {
 	const router = useRouter();
@@ -29,7 +25,8 @@ const ExchangeConnection = () => {
 
 	const [exchangeOptions, setExchangeOptions] = useState<ISelectBoxOption[]>([]);
 	const tabs = [{ label: "Manual Connection" }, { label: "Fast Connection" }];
-	const ipAddress = "2345678901mj940485686505940400";
+	const ipAddress = ["2345678901mj940485686505940400", "940485686505940l4002345678901m"];
+	const ipString = ipAddress.join(", ");
 
 	const { data: exchanges, isSuccess: isExchangeSuccess } = useExchanges({
 		status: TradeStatus.active,
@@ -55,99 +52,6 @@ const ExchangeConnection = () => {
 
 	const handleSuccessClose = () => setToggleSuccess(false);
 
-	const handleExchangeCopy = () => {
-		navigator.clipboard
-			.writeText(ipAddress)
-			.then(() => {
-				console.log("IP address copied to clipboard");
-			})
-			.catch((err) => {
-				console.error("Failed to copy IP address: ", err);
-			});
-	};
-
-	const ManualConnection = () => (
-		<div className="flex flex-col gap-y-4">
-			<SelectBox
-				option={selectedExchange}
-				isSearchable
-				labelText="Exchange"
-				options={exchangeOptions}
-				placeholder="Select Exchange Channel"
-				setOption={setSelectedExchange}
-			/>
-			<InputField
-				type="text"
-				labelText="API Keys"
-				props={{ name: "apikey" }}
-				placeholder="Enter API Keys"
-				onChange={setApiKey}
-			/>
-			<InputField
-				type="text"
-				labelText="Secret Keys"
-				props={{ name: "secretkey" }}
-				placeholder="Enter Secret Keys"
-				onChange={setSecretKey}
-			/>
-			<div>
-				<h3 className="text-gray-950 text-base font-bold uppercase">Important</h3>
-				<span className="text-zinc-500 text-sm font-bold leading-tight">
-					You must add TraderApp IP Address from below to your list of trusted IPs.
-					Otherwise, if there are some days of inactivity, your IP key will be deleted by
-					the exchange.
-				</span>
-			</div>
-			<InputField
-				type="text"
-				icon={{ name: <ExchangeCopyIcon />, onClick: handleExchangeCopy }}
-				labelText="IP Address"
-				props={{ name: "ipaddress", disabled: true }}
-				placeholder={ipAddress}
-			/>
-			<Button
-				disabled={isSubmitDisabled}
-				type="submit"
-				fluid
-				className="mt-2 flex justify-center"
-				innerClassName="px-[20%] py-4 capitalize"
-			>
-				Connect
-			</Button>
-			<Link className="mt-2 flex justify-center text-blue-800" href={""}>
-				How to Connect
-			</Link>
-		</div>
-	);
-
-	const FastConnection = () => (
-		<div>
-			<h3 className="text-center text-zinc-500 text-sm font-bold leading-tight px-1.5">
-				TraderApp will receive access to your trading history and balance and will get the
-				ability to replace orders on the exchange.
-			</h3>
-			<OrderedListTile
-				items={[
-					"Click the “connect” button",
-					"Log in to your Binance Account",
-					"Confirm your connection to TraderApp",
-				]}
-			/>
-			<Button
-				type="submit"
-				fluid
-				className="mt-2 flex justify-center"
-				innerClassName="px-[20%] py-4"
-			>
-				Connect
-			</Button>
-			<Link className="my-8 flex justify-center text-blue-800" href={""}>
-				<span className="text-base font-medium mr-1">Don't have an account? </span>
-				<span className="text-base font-bold">Create one now.</span>
-			</Link>
-		</div>
-	);
-
 	return (
 		<>
 			<Modal
@@ -158,7 +62,15 @@ const ExchangeConnection = () => {
 			>
 				<ExchangeTile imageUrl={selectedExchange?.imgUrl} />
 				<ContentTab tabs={tabs}>
-					<ManualConnection />
+					<ManualConnection
+						selectedExchange={selectedExchange}
+						exchangeOptions={exchangeOptions}
+						setSelectedExchange={setSelectedExchange}
+						setApiKey={setApiKey}
+						setSecretKey={setSecretKey}
+						isSubmitDisabled={isSubmitDisabled}
+						ipString={ipString}
+					/>
 					<FastConnection />
 				</ContentTab>
 				<MessageModal
