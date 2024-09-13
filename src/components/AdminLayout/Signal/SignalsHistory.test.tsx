7d8cf51d029055dc8f39e8f6021c250fb6d2f73d
Ndife Uchenna <32971118+Ndife@users.noreply.github.com>
2024-09-13 19:49:10 +0100

    move signals history test data to external file

--- src/components/AdminLayout/Signal/SignalsHistory.test.tsx
+++ src/components/AdminLayout/Signal/SignalsHistory.test.tsx
@@ -1,122 +1,19 @@
 import React from "react";
 import { render, screen, waitFor } from "@testing-library/react";
-import "@testing-library/jest-dom";
 import { useSignalHistory } from "~/apis/handlers/assets/hooks";
 import SignalsHistory from "./SignalsHistory";
-import { Candlestick, SignalRisk, SignalStatus } from "~/apis/handlers/assets/enums";
 import {
 	signalsHistoryDataTableMobileSelector,
 	signalsHistoryDataTableSelector,
 } from "~/selectors/signals";
 import { format } from "date-fns";
+import { signalData } from "./SignalData";
 
 // Mock the useSignalHistory hook
 jest.mock("~/apis/handlers/assets/hooks");
 
 // Mock implementation of useSignalHistory
 const mockUseSignalHistory = useSignalHistory as jest.MockedFunction<typeof useSignalHistory>;
-const signalHistory = [
-	{
-		id: "66bb7135e59f0544faca48fa",
-		targetProfits: [
-			{
-				price: 200,
-				percent: 1,
-				isReached: false,
-			},
-			{
-				price: 300,
-				percent: 2,
-				isReached: false,
-			},
-		],
-		stopLoss: {
-			price: 1000,
-			percent: 1,
-			isReached: false,
-		},
-		entryPrice: 8000,
-		tradeNote: "first signal",
-		candlestick: Candlestick.oneHour,
-		risk: SignalRisk.low,
-		isSignalTradable: true,
-		chartUrl:
-			"https://aws-s3-dev-assets-service.s3.eu-west-1.amazonaws.com/charts/f9b98a21-d47a-45e2-9c19-6c9ab48c1c0a.png",
-		status: SignalStatus.INACTIVE,
-		createdAt: "2024-08-13T14:44:05.376Z",
-		endedAt: "2024-08-13T14:44:05.376Z",
-		maxGain: 0,
-		asset: {
-			id: "52",
-			name: "XRP",
-			symbol: "XRP",
-			logo: "https://s2.coinmarketcap.com/static/img/coins/64x64/52.png",
-		},
-		baseCurrency: {
-			id: "52",
-			name: "XRP",
-			symbol: "XRP",
-			logo: "https://s2.coinmarketcap.com/static/img/coins/64x64/52.png",
-		},
-		supportedExchanges: [
-			{
-				_id: "270",
-				name: "Binance",
-				logo: "https://s2.coinmarketcap.com/static/img/exchanges/64x64/270.png",
-			},
-		],
-	},
-	{
-		id: "66bb7135e59f0544faca48fb",
-		targetProfits: [
-			{
-				price: 400,
-				percent: 1,
-				isReached: false,
-			},
-			{
-				price: 500,
-				percent: 2,
-				isReached: false,
-			},
-		],
-		stopLoss: {
-			price: 5000,
-			percent: 1,
-			isReached: false,
-		},
-		entryPrice: 8000,
-		tradeNote: "second signal",
-		candlestick: Candlestick.oneHour,
-		risk: SignalRisk.low,
-		isSignalTradable: true,
-		chartUrl:
-			"https://aws-s3-dev-assets-service.s3.eu-west-1.amazonaws.com/charts/f9b98a21-d47a-45e2-9c19-6c9ab48c1c0a.png",
-		status: SignalStatus.INACTIVE,
-		createdAt: "2024-08-13T14:44:05.376Z",
-		endedAt: "2024-08-13T14:44:05.376Z",
-		maxGain: 0,
-		asset: {
-			id: "52",
-			name: "BTC",
-			symbol: "BTC",
-			logo: "https://s2.coinmarketcap.com/static/img/coins/64x64/52.png",
-		},
-		baseCurrency: {
-			id: "52",
-			name: "BTC",
-			symbol: "BTC",
-			logo: "https://s2.coinmarketcap.com/static/img/coins/64x64/52.png",
-		},
-		supportedExchanges: [
-			{
-				_id: "270",
-				name: "Binance",
-				logo: "https://s2.coinmarketcap.com/static/img/exchanges/64x64/270.png",
-			},
-		],
-	},
-];
 
 describe("SignalsHistory Component", () => {
 	beforeEach(() => {
@@ -128,7 +25,7 @@ describe("SignalsHistory Component", () => {
 		mockUseSignalHistory.mockReturnValue({
 			isLoading: false,
 			isSuccess: true,
-			signalHistory,
+			signalHistory: signalData,
 			signalsTableHead: [],
 			signalsTableBody: undefined,
 			signalsMobileTableBody: [],
@@ -181,13 +78,13 @@ describe("SignalsHistory Component", () => {
 	});
 
 	test("renders DataTable and DataTableMobile when signal history exists", async () => {
-		const { tableHead, tableBody } = signalsHistoryDataTableSelector(signalHistory);
-		const dataMobile = signalsHistoryDataTableMobileSelector(signalHistory);
+		const { tableHead, tableBody } = signalsHistoryDataTableSelector(signalData);
+		const dataMobile = signalsHistoryDataTableMobileSelector(signalData);
 
 		mockUseSignalHistory.mockReturnValue({
 			isLoading: false,
 			isSuccess: true,
-			signalHistory,
+			signalHistory: signalData,
 			signalsTableHead: tableHead,
 			signalsTableBody: tableBody,
 			signalsMobileTableBody: dataMobile,
@@ -206,7 +103,7 @@ describe("SignalsHistory Component", () => {
 
 		// Wait for data to be rendered
 		await waitFor(() => {
-			signalHistory.forEach((signal) => {
+			signalData.forEach((signal) => {
 				expect(tableData).toHaveTextContent(signal.asset.name);
 				expect(tableData).toHaveTextContent(signal.risk);
 				expect(tableData).toHaveTextContent(
@@ -220,13 +117,13 @@ describe("SignalsHistory Component", () => {
 	});
 
 	test("pagination works correctly", () => {
-		const { tableHead, tableBody } = signalsHistoryDataTableSelector(signalHistory);
-		const dataMobile = signalsHistoryDataTableMobileSelector(signalHistory);
+		const { tableHead, tableBody } = signalsHistoryDataTableSelector(signalData);
+		const dataMobile = signalsHistoryDataTableMobileSelector(signalData);
 
 		mockUseSignalHistory.mockReturnValue({
 			isLoading: false,
 			isSuccess: true,
-			signalHistory,
+			signalHistory: signalData,
 			signalsTableHead: tableHead,
 			signalsTableBody: tableBody,
 			signalsMobileTableBody: dataMobile,
