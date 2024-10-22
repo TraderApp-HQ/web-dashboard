bba18113528bb669555a7402606d6ef537aa76a5
SpiffGreen <spiffjekeygreen@gmail.com>
2024-10-22 07:11:05 +0100

    fix: signup with/without referralCode issue
    Also, return referralRank on getting referrals

--- src/selectors/referrals/constants.ts
+++ src/selectors/referrals/constants.ts
@@ -1,9 +1,9 @@
 import { ITHead } from "~/components/common/DataTable/config";
 
 export const UsersCommunityHeadItems: ITHead[] = [
-	{ displayItem: "Full Name" },
-	{ displayItem: "Current Rank" },
-	{ displayItem: "Level in Referral Tree" },
-	{ displayItem: "Status" },
-	{ displayItem: "Registration Date" },
+	{ displayItem: "Full Name", styles: "text-center" },
+	{ displayItem: "Current Rank", styles: "text-center" },
+	{ displayItem: "Level in Referral Tree", styles: "text-center" },
+	{ displayItem: "Status", styles: "text-center" },
+	{ displayItem: "Registration Date", styles: "text-center" },
 ];
