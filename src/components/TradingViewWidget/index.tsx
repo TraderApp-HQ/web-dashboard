f80dd67523d9471c3d833990166bec1f761050e6
Ogheneochuko Dick Duvwarovwo <117981983+dkey01@users.noreply.github.com>
2024-10-30 09:45:27 +0100

    [TA-84]TradingViewChart (#35)
    * TradingViewChart
    
    * Updated
    
    * Updated
    
    * Updated
    
    * Updated
    
    * Updated
    
    * Updated
    
    * Updated
    
    ---------
    
    Co-authored-by: Dike Obinna Ajah <52049352+ekidhaja@users.noreply.github.com>

---
+++ src/components/TradingViewWidget/index.tsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useRef, memo } from "react";
+
+interface Asset {
+	asset: {
+		id: string;
+		name: string;
+		symbol: string;
+		logo: string;
+	};
+}
+
+interface TradingViewWidgetProps {
+	signal: Asset;
+}
+const TradingViewWidget: React.FC<TradingViewWidgetProps> = ({ signal }) => {
+	const container = useRef<HTMLDivElement | null>(null);
+
+	useEffect(() => {
+		// Clear previous script instance if exists
+		if (container.current) {
+			container.current.innerHTML = "";
+		}
+
+		const script = document.createElement("script");
+		script.src = "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
+		script.type = "text/javascript";
+		script.async = true;
+		script.innerHTML = `
+        {
+          "autosize": true,
+          "symbol": "${signal?.asset?.symbol}",
+          "interval": "D",
+          "timezone": "Etc/UTC",
+          "theme": "light",
+          "style": "1",
+          "locale": "en",
+          "allow_symbol_change": false,
+          "calendar": false,
+          "support_host": "https://www.tradingview.com"
+        }`;
+		container.current?.appendChild(script);
+	}, [signal?.asset?.id]);
+
+	return (
+		<div className="w-full h-[30rem]" ref={container}>
+			<div className=" w-full h-[10rem]"></div>
+			<div>
+				<a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank">
+					<span className="blue-text">Track all markets on TradingView</span>
+				</a>
+			</div>
+		</div>
+	);
+};
+
+export default memo(TradingViewWidget);
--- src/pages/account/signals/active/[id]/live_chart/index.tsx
+++ src/pages/account/signals/active/[id]/live_chart/index.tsx
@@ -1,31 +1,50 @@
 /* eslint-disable @next/next/no-img-element */
-import { getAsset } from "~/lib/utils";
-import data from "~/pages/account/signals/data.json";
-import type { Signal } from "~/lib/types";
-import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import AssetBreakdown from "~/pages/account/signals/active/[id]";
+import TradingViewWidget from "~/components/TradingViewWidget";
+import { useFetchActiveSignals } from "~/apis/handlers/assets/hooks";
+import { ISignal } from "~/apis/handlers/assets/interfaces";
+import { useRouter } from "next/router";
+
+interface Signal {
+	asset: {
+		id: string;
+		name: string;
+		symbol: string;
+		logo: string;
+	};
+}
 
 const LiveChart = () => {
+	const { isLoading, isSuccess, activeSignals } = useFetchActiveSignals({});
+	const [selectedsignal, setSelectedsignal] = useState<Signal | ISignal>({
+		asset: {
+			id: "",
+			name: "",
+			symbol: "",
+			logo: "",
+		},
+	});
 	const router = useRouter();
+
 	const id = router.query.id as string;
-	const [asset, setAsset] = useState<Signal | null>(null);
 
 	useEffect(() => {
-		async function fetchData() {
-			const asset: Signal | null = await getAsset("1", data);
-			setAsset(asset);
+		if (!isLoading && isSuccess) {
+			const signalAsset = activeSignals.filter((asset) => asset.id === id);
+			setSelectedsignal(signalAsset[0]);
 		}
-		fetchData();
-	}, [id]);
+	}, [isLoading, isSuccess, activeSignals]);
 
 	return (
 		<>
-			<div className="sm:w-[100%] grid gap-y-8">
-				<div className="flex justify-center rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl">
-					<img src={asset?.signalImage ?? ""} className="w-full" alt="signal chart" />
+			{!isLoading && isSuccess && (
+				<div className="sm:w-[100%] grid gap-y-8">
+					<div className="flex justify-center rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl">
+						<TradingViewWidget signal={selectedsignal} />
+					</div>
 				</div>
-			</div>
+			)}
 		</>
 	);
 };
