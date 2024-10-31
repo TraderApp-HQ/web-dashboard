1bc2bff092635ab955f7734829bfa24b41607cc5
Ogheneochuko Dick Duvwarovwo <ochukodick@gmail.com>
2024-10-31 10:31:25 +0100

    Updated

--- src/components/TradingViewWidget/index.tsx
+++ src/components/TradingViewWidget/index.tsx
@@ -3,9 +3,10 @@ import React, { useEffect, useRef, memo } from "react";
 interface Asset {
 	asset: {
 		id: string;
-		name: string;
 		symbol: string;
-		logo: string;
+	};
+	baseCurrency: {
+		symbol: string;
 	};
 }
 
@@ -28,7 +29,7 @@ const TradingViewWidget: React.FC<TradingViewWidgetProps> = ({ signal }) => {
 		script.innerHTML = `
         {
           "autosize": true,
-          "symbol": "${signal?.asset?.symbol}",
+          "symbol": "${signal?.asset?.symbol}${signal?.baseCurrency?.symbol}",
           "interval": "D",
           "timezone": "Etc/UTC",
           "theme": "light",
@@ -42,7 +43,7 @@ const TradingViewWidget: React.FC<TradingViewWidgetProps> = ({ signal }) => {
 	}, [signal?.asset?.id]);
 
 	return (
-		<div className="w-full h-[30rem]" ref={container}>
+		<div className="w-full h-[580px]" ref={container}>
 			<div className=" w-full h-[10rem]"></div>
 			<div>
 				<a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank">
