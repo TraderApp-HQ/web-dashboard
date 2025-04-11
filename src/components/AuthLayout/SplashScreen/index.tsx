57ea6337af0bcc8fdd7231fd4b63aac6cddcae7b
Fisayo <aikomofisayo@gmail.com>
2025-04-11 09:29:11 +0100

    Refactor SplashScreen component and add TraderAppIllustration; update TraderAppLogoWhite to accept width and height props

--- src/components/AuthLayout/SplashScreen/index.tsx
+++ src/components/AuthLayout/SplashScreen/index.tsx
@@ -1,14 +1,14 @@
 import React from "react";
+import TraderAppIllustration from "~/components/icons/TraderAppIllustration";
 import TraderAppLogoPale from "~/components/icons/TraderAppLogoWhite";
-import TraderAppWalletPageSample from "~/components/icons/TraderAppWalletPageSample";
 
 const SplashScreen = () => {
 	return (
 		<div className="flex items-center justify-center bg-[#102477] h-full text-white">
 			<div className="flex flex-col justify-center items-center space-y-12">
-				<TraderAppLogoPale />
+				<TraderAppLogoPale width={260} height={56} />
 				<div className="flex flex-col items-center justify-center space-y-10">
-					<TraderAppWalletPageSample />
+					<TraderAppIllustration />
 					<div>
 						<h3 className="text-[#F0F5FF] text-[32px] font-bold">Easy trading</h3>
 						<p className="text-[#E1E6EF] text-sm font-normal">
