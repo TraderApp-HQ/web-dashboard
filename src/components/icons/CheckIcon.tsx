6542f35c0211c32148d7bba47b23ce120e457843
Samuel Ezeh <samuelblessed38@gmail.com>
2025-09-12 09:30:48 +0100

    [fix: TA-316] Icon fix

--- src/components/icons/CheckIcon.tsx
+++ src/components/icons/CheckIcon.tsx
@@ -4,17 +4,17 @@ function CheckIcon(props: SVGAttributes<SVGElement>) {
 	return (
 		<svg
 			width="20"
-			height="21"
+			height="20"
 			viewBox="0 0 20 21"
 			fill="none"
 			xmlns="http://www.w3.org/2000/svg"
 			{...props}
+			className="rounded-full"
 		>
-			<rect x="0.5" y="1" width="19" height="19" rx="5.5" fill="#F9F5FF" />
-			<rect x="0.5" y="1" width="19" height="19" rx="5.5" stroke="#1836B2" />
+			<circle cx="10" cy="10.5" r="9.5" fill="#22C55E" stroke="#fff" />
 			<path
 				d="M14.6667 7L8.25004 13.4167L5.33337 10.5"
-				stroke="#1836B2"
+				stroke="#fff"
 				strokeWidth="2"
 				strokeLinecap="round"
 				strokeLinejoin="round"
